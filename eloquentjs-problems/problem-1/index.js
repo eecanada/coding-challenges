cc91c93bf31c8ae991f0b1bac054c3624d1a06de
Eder Canada <canada.eder@gmail.com>
2019-11-27 13:25:52 -0800

    problem 1 solved

---
+++ eloquentjs-problems/problem-1/index.js
@@ -0,0 +1,9 @@
+// Consider a program that outputs all even numbers from 0 to 12 
+
+
+// the while loop is only going to keep executing the code as long as the condition is true 
+let num = 0 
+while (num <= 12){
+  console.log(num)
+  num = num + 2 
+}

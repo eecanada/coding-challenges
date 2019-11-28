8f5423f426ca7574d775fc0cf9ce93c10dd2b29a
Eder Canada <canada.eder@gmail.com>
2019-11-27 22:59:51 -0800

    solved problem 2

--- project-euler/problem-2/index.js
+++ project-euler/problem-2/index.js
@@ -5,23 +5,26 @@
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
 let sum = 0
+
 function getSum(){
   let num1 = 1;
   let num2 = 2;
-  while(i < 4000000 ){
+
+  while(sum < 4000000 ){
+
     if(num1 % 2 === 0) {
-      console.log('this is sum', sum+=num1)
+      console.log('this is sum', sum = sum + num1)
     }
     if(num2 % 2 === 0){
-      console.log('this is sum', sum+=num2)
+      console.log('this is sum', sum = sum + num2)
     }
-    num1+= num2
-    num2+= num1
+  
+    num1 = num1 + num2
+    num2 = num2 + num1
    }
 }
 getSum()
 
-while(let i = 0; i < 4000000; i ++)
 
 
 

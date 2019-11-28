5ff96600bc447f5f4ebfd9d57594d8dbc526567d
Eder Canada <canada.eder@gmail.com>
2019-11-27 22:35:32 -0800

    solve fizzbuzz problem

--- eloquentjs-problems/problem-2/index.js
+++ eloquentjs-problems/problem-2/index.js
@@ -1,4 +1,4 @@
-// Write a loop that makes seven calls to console.log to output the following triangle:
+//Looping a triangle -  Write a loop that makes seven calls to console.log to output the following triangle:
 
 // #
 // ##

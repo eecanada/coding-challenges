526d6badfabb76a6c5f7ac62ffd349930f2d940a
Eder Canada <canada.eder@gmail.com>
2019-11-28 23:01:56 -0800

    comment on how to solve problem 2

--- ga-recursion/problem-2/index.js
+++ ga-recursion/problem-2/index.js
@@ -6,9 +6,11 @@ function sumArray(arr, index = 0, sum = 0){
   if(index === arr.length){
     return sum;
   }
-  sum += arr[index];
+  sum = sum + arr[index];
   return sumArray(arr, index + 1, sum)
 }
 
 
 console.log(sumArray(arr));
+
+// solved this by setting up my parameters of arr, index, and sum, then set up the base case, at action was that if the base case was not met, then the sum should equal to current sum plus current value of the index, and finally the recursive action is to add +1 index to move through the array

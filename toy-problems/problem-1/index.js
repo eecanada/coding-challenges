a36d67b82dcdc82d1e9215d99e119b30ebb3d5e1
Eder Canada <canada.eder@gmail.com>
2019-12-29 09:41:03 -0800

    reverse string problem solved

---
+++ toy-problems/problem-1/index.js
@@ -0,0 +1,11 @@
+// Reverse a string
+
+let str = 'eder'
+
+str = str.split('')
+console.log(str) // ['e','d','e','r']
+str = str.reverse()
+console.log(str) // ['r','e','d','e']
+str = str.join('')
+console.log(str) // rede
+console.log(typeof(str))

56900033b5e95305d0e961d365269ad9bd94313f
Eder Canada <canada.eder@gmail.com>
2020-01-28 20:27:54 -0800

    problem 9 part 1 corrected

--- eloquentjs-problems/problem-9/index.js
+++ eloquentjs-problems/problem-9/index.js
@@ -2,11 +2,23 @@
 
 const reverseArray = (arr) => {
   const newArr = []
-  for (let i = arr.length; i > 0; i--){
-    newArr.push(i)
+  for (let i = arr.length - 1 ; i >= 0; i--){
+    newArr.push(arr[i])
   }
   console.log(newArr)
 }
 
-const arr = [1,2,3,4,5]
+const arr = [2,3,4,5,6]
 reverseArray(arr)
+
+
+// The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+// const reverseArrayInPlace = (arr) => {
+//   for (let i = arr.length; i > 0; i--){
+//     console.log(arr)
+//   }
+// }
+
+// const arr = [1,2,3,4,5]
+// reverseArrayInPlace(arr)

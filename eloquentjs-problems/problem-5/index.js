0cc0789587c2bca435b983ce3d4f658273da2084
Eder Canada <canada.eder@gmail.com>
2020-05-18 20:28:51 -0700

    add solution

--- eloquentjs-problems/problem-5/index.js
+++ eloquentjs-problems/problem-5/index.js
@@ -1,19 +1,20 @@
 // Write a function min that takes two arguments and returns their minimum.
 
-// solution 1
+// solution 1 function decleration 
 function smallestNumber (num1, num2){
   return Math.min(num1,num2)
 
 }
 console.log(smallestNumber(3,9))
 
-// solution 2
-function smallerNumber (num3, num4){
-  if(num3 < num4){
-    return num3
-  } else if (num4 < num3){
-    return num4
-  }
+// solution 2 function expression
+const smallestNumber = function (a,b){
+  return Math.min(a,b)
 }
+const num = smallestNumber(5,6)
+console.log(num)
 
-console.log(smallestNumber(12,5))
+
+
+//solution 3 arrow function
+smallNum = (a,b) => Math.min(a,b)

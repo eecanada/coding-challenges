472dff82a7fba104cf2ce5e92882bcb77140fc45
Eder Canada <canada.eder@gmail.com>
2020-05-19 21:49:45 -0700

    add new solution

--- eloquentjs-problems/problem-8/index.js
+++ eloquentjs-problems/problem-8/index.js
@@ -1,18 +1,32 @@
+// You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
 // Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
 
-// console.log("string"[3]) // --> 'i'
+// Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
 
-
-function countBs (str) {
+function countChar (str,strChar) {
   const splitStr = str.split('')
-  const Bees = []
+  const charOfStr = []
   for(let i = 0; i < splitStr.length; i++){
-    if(splitStr[i] === 'B'){
-      Bees.push(splitStr[i])
+    if(splitStr[i] === strChar){
+      charOfStr.push(splitStr[i])
     }
   }
-  console.log(Bees.length)
+  console.log(charOfStr.length)
 }
 
-countBs('BaBs')
+countChar('bubbles', 'b')
+
+
+//alternate solution
+function countChar(str,letter){
+  strArr = str.split('')
+  count = []
+  for(let i = 0; i<strArr.length ; i++){
+    if(strArr[i] === letter){
+      count.push(strArr[i])
+    }
+  }
+  return count.length
+}
+console.log(countChar('eder','e'))

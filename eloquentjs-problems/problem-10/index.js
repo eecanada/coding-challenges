45c79c3a929f7d330586ee28d2d2ffc82aa8ca24
Eder Canada <canada.eder@gmail.com>
2020-05-19 21:24:37 -0700

    solve problem 6 recursive

--- eloquentjs-problems/problem-10/index.js
+++ eloquentjs-problems/problem-10/index.js
@@ -1,20 +1,45 @@
-// Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties,
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+function arrList(start, end){
+  const numbers = []
+  for(let i = start; i <= end; i++){
+    numbers.push(i)
+  }
+  return numbers
+}
+
+const numArr = arrList(1,10)
+console.log(numArr) 
 
 
-function deepEqual(obj, obj){
-  if (obj.value === obj.value){
-    console.log('true')
-  } else if (obj === {here: 1, object:2}) {
-    console.log('false')
+
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
+function addArr(arr){
+  sum = 0
+  for(let i = 0; i < arr.length; i++){
+    sum = arr[i] + sum
+  }
+  console.log(sum)
+}
+
+addArr(numArr)
+
+
+
+//modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior.
+function arrList(start, end, step){
+  const numbers = []
+  if(step == null){
+    for(let i = start; i <= end; i++){
+      numbers.push(i)
+    }
+    return numbers 
+  } else {
+    for(let i = start; i <= end; i = i + step){
+      numbers.push(i)
+    }
+    return numbers
   }
 }
 
-const obj = {
-  here: {is: 'an'}, 
-  object:2
-}
-
-console.log(deepEqual(obj, {here: 1, object: 2}));
-
-console.log(obj.here, 'yo')
-console.log(obj.object, 'hi')
+const numArr = arrList(1,10)
+console.log(numArr) 
---
+++ eloquentjs-problems/problem-11/index.js
@@ -0,0 +1,14 @@
+// The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. 
+
+// const reverseArray = (arr) => {
+//   const newArr = []
+//   for (let i = arr.length - 1 ; i >= 0; i--){
+//     newArr.push(arr[i])
+//   }
+//   console.log(newArr)
+// }
+
+// const arr = [2,3,4,5,6]
+// reverseArray(arr)
+
+

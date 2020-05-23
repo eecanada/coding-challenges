f37adc58bfd98317e8974e8bed8633594563dc2c
Eder Canada <canada.eder@gmail.com>
2020-05-22 22:06:30 -0700

    problem 10 part 1 solved

--- eloquentjs-problems/problem-10/index.js
+++ eloquentjs-problems/problem-10/index.js
@@ -1,22 +1,13 @@
-// The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. 
-
-// const reverseArray = (arr) => {
-//   const newArr = []
-//   for (let i = arr.length - 1 ; i >= 0; i--){
-//     newArr.push(arr[i])
-//   }
-//   console.log(newArr)
-// }
-
-// const arr = [2,3,4,5,6]
-// reverseArray(arr)
+// The first, reverseArray, takes an array as argument
+// and produces a new array that has the same elements in the inverse order.
 
 
-// Arrays have a reverse method that changes the array by inverting the order in
-// which its elements appear. For this exercise, write two functions, reverseArray
-// and reverseArrayInPlace. The first, reverseArray, takes an array as argument
-// and produces a new array that has the same elements in the inverse order. The
-// second, reverseArrayInPlace, does what the reverse method does: it modifies
-// the array given as argument by reversing its elements. Neither may use the
-// standard reverse method.
+let reverseArray = arr => {
+  let newArr = []
+  for (let i = arr.length-1; i >= 0; i--) {
+    newArr.push(arr[i])
+  }
+  return newArr
+}
 
+console.log(reverseArray([1,2,3]))

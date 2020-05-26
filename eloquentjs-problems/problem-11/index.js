c099293f126a755bac01de540f3481a89b08ffc0
Eder Canada <canada.eder@gmail.com>
2020-05-25 23:15:03 -0700

    problem 11 part 2

--- eloquentjs-problems/problem-11/index.js
+++ eloquentjs-problems/problem-11/index.js
@@ -5,6 +5,7 @@
 function arrayToList(...arr) {
   let list = null;
   for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i], 'hit')
     list = {value: arr[i], rest: list};
   }
   return list;
@@ -12,3 +13,17 @@ function arrayToList(...arr) {
 
 console.log(arrayToList(1,2,3))
 
+
+//Also write a listToArray function that produces an array from a list.
+
+
+let list = { value: 1, 
+  rest: { value: 2, 
+    rest: { value: 3, 
+      rest: null } } }
+
+const listToArray = (list) => {
+  console.log(list)
+}
+
+listToArray(list)

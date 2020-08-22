816e559d0040138ec286f998f505e3580849d73f
Eder Canada <canada.eder@gmail.com>
2020-08-22 16:34:42 -0700

    promblem 8 part 2

--- toy-problems/problem-8/index.js
+++ toy-problems/problem-8/index.js
@@ -4,3 +4,9 @@ let [Ann, Fred, John] = names;
 console.log(John);
 
 //Ann
+
+
+//What is the output of the following code?
+let nums = [3, 4, 5];
+let all = [1, 2, ...nums, 6];
+console.log(all[3]);// 4

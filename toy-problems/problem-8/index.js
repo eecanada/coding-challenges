32405767b7ea7109c2603aba86da53266471baee
Eder Canada <canada.eder@gmail.com>
2020-08-23 17:58:00 -0700

    problem 8 part 3

--- toy-problems/problem-8/index.js
+++ toy-problems/problem-8/index.js
@@ -5,8 +5,16 @@ console.log(John);
 
 //Ann
 
-
 //What is the output of the following code?
 let nums = [3, 4, 5];
 let all = [1, 2, ...nums, 6];
 console.log(all[3]);// 4
+
+
+
+
+function add (a = 0, b = 10) {
+  return a + b;
+}
+const sum = add(10);
+console.log(sum); //--> 20

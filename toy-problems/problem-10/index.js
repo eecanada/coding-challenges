3c822daf4aa4bae2742c3c430fd22729f56e84f4
Eder Canada <canada.eder@gmail.com>
2022-01-31 11:32:52 -0800

    array of arrays to array of objects

---
+++ toy-problems/problem-10/index.js
@@ -0,0 +1,12 @@
+// Turn this array of arrays into an array of objects
+
+const classes = [
+  [ 'Chemistry', '9AM', 'Mr. Darnick' ],
+  [ 'Physics', '10:15AM', 'Mrs. Lithun'],
+  [ 'Math', '11:30AM', 'Mrs. Vitalis' ]
+];
+
+const classesAsObject = classes.map(([subject, time, teacher]) => {
+    return {subject, time, teacher}
+})
+
